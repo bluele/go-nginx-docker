8092c20529c724111efef31545c65ccb1667d554
Jun Kimura <jksmphone@gmail.com>
2016-02-15 23:58:18 +0900

    add misc files.

--- main.go
+++ main.go
@@ -3,19 +3,29 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/garyburd/redigo/redis"
 )
 
 func main() {
-	redi, err := redis.Dial("tcp", "redis:6379")
+	socketPath := "/tmp/app.sock"
+	l, err := net.ListenUnix("unix", &net.UnixAddr{socketPath, "unix"})
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	defer os.Remove(socketPath)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		redi, err := redis.Dial("tcp", "redis:6379")
+		if err != nil {
+			log.Fatal(err)
+		}
 		res, err := redi.Do("incr", "counter")
 		if err != nil {
 			w.WriteHeader(500)
@@ -30,5 +40,21 @@ func main() {
 			w.Write([]byte("unexpected value"))
 		}
 	})
-	log.Fatal(http.ListenAndServe(":5000", nil))
+
+	log.Println("Start server...")
+	log.Fatal(http.Serve(l, http.DefaultServeMux))
+}
+
+func listFiles(path string) {
+	list, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+	for _, finfo := range list {
+		if finfo.IsDir() || -1 == strings.Index(finfo.Name(), ".txt") {
+			continue
+		}
+		fmt.Printf("%q\n", finfo.Name())
+	}
 }
